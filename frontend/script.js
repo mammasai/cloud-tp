38fdb75b417c2ea43efc85f647444dc24c22b13e
mammasai <mammasai4@gmail.com>
2026-04-13 19:24:31 +0100

    connect frontend to cloud

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
-const API = "http://127.0.0.1:5000";
-
+//const API = "http://127.0.0.1:5000";
+const API = "https://cloud-tp-ydkb.onrender.com";
 function addSQL() {
     const name = document.getElementById("name").value;
 
